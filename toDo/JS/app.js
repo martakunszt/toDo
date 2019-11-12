d9c4b8edda7802a09574deadbe3813a3afb171fd
martakunszt <kunsztowicz.m@gmail.com>
2019-11-12 11:09:34 +0000

    add remove function - only removes index = 0

--- toDo/JS/app.js
+++ toDo/JS/app.js
@@ -2,9 +2,11 @@
 
 let button = document.getElementById("add");
 let textBox = document.getElementById("input");
+let remove = document.getElementById("delete");
+
 
 function addItem (){
-    alert("I'm adding an item");
+    //alert("I'm adding an item");
     let item = textBox.value; 
     let listItem = document.createElement("li");
     let list = document.getElementById("todo");
@@ -16,3 +18,20 @@ button.addEventListener("click",addItem);
 
 
 
+let items = document.querySelectorAll("#todo li"),
+tab = [], liIndex;
+
+for(var i = 0; i<items.length; i++){
+    tab.push(items[i].innerHTML);
+}
+
+for(var i = 0; i < items.length; i++){
+    items[i].onclick = function(){
+        liIndex = tab.indexOf(this.innerHTML)
+        console.log(this.innerHTML + "INDEX = " +liIndex)
+    };
+}
+
+function removeLI(){
+    items[liIndex].parentNode.removeChild(items[liIndex]);
+}
